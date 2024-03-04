4aa2ea983f546b4f1aaaa8683860e7e63ec3ebd3
SergGrishkov <serg.grishkov@gmail.com>
2024-03-04 23:06:13 +0200

    Add Search bar

---
+++ src/components/App/App.jsx
@@ -0,0 +1,92 @@
+import { useState, useEffect } from "react";
+import Modal from "react-modal";
+import SearchBar from "../SearchBar/SearchBar";
+import clsx from "clsx";
+
+// import ErrorMessage from "./error-message/ErrorMessage";
+// import ImageGallery from "./image-gallery/ImageGallery";
+// import Loader from "./loader/Loader";
+// import LoadMoreBtn from "./loadmore-btn/LoadMoreBtn";
+// import ImageModal from "./Image-modal/ImageModal";
+
+import { getPictures } from "../../api/controller";
+import styles from "./App.module.css";
+
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [currPage, setCurrPage] = useState(0);
+  const [hasMorePages, setHasMorePages] = useState(false);
+  const [filter, setFilter] = useState("");
+
+  // const [selectedImage, setSelectedImage] = useState(null);
+  // const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  useEffect(() => {
+    Modal.setAppElement("#root");
+  }, []);
+
+  const updateImages = async (strFilter, page) => {
+    try {
+      setError(false);
+      setLoading(true);
+      const data = await getPictures(strFilter, page);
+      if (data.results.length > 0) {
+        setItems((prevItems) => [...prevItems, ...data.results]);
+        setCurrPage(page);
+        setHasMorePages(page >= data.total_pages ? false : true);
+      } else {
+        setError(true);
+        setHasMorePages(false);
+      }
+    } catch (error) {
+      setError(true);
+      setHasMorePages(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSearch = (strFilter) => {
+    setFilter(strFilter);
+    setItems([]);
+    setCurrPage(0);
+    setHasMorePages(false);
+    updateImages(strFilter, 1);
+  };
+
+  // const handleMore = () => updateImages(filter, currPage + 1);
+
+  // const openModal = (image) => {
+  //   setSelectedImage(image);
+  //   setModalIsOpen(true);
+  // };
+
+  // const closeModal = () => {
+  //   setModalIsOpen(false);
+  // };
+
+  return (
+    <div className={styles.container}>
+      <SearchBar onSearch={handleSearch} />
+      <div className={clsx(styles.content, styles.section)}>
+        {/* <ErrorMessage isError={error} />
+        <ImageGallery images={items} openModal={openModal} />
+        <Loader isLoading={loading} /> */}
+
+        {/* <LoadMoreBtn
+          isVisible={hasMorePages && !loading}
+          onClick={handleMore}
+        ></LoadMoreBtn> */}
+      </div>
+      {/* <ImageModal
+        isOpen={modalIsOpen}
+        image={selectedImage}
+        onCloseClick={closeModal}
+      /> */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/notification/messages.js
@@ -0,0 +1,20 @@
+import toast from "react-hot-toast";
+
+export const errorMessage = {
+  emptySearch: "It is necessary to enter text for images search.",
+  emptyLoad: "Whoops, something went wrong! Please try reloading this page!",
+  loadMore: "Load More",
+};
+
+export const errNotify = (message) => {
+  toast.error(message, {
+    duration: 500,
+  });
+};
+
+
+export const okNotify = (message) => {
+  toast.success(message, {
+    duration: 500,
+  });
+};
