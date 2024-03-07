90df95e1b861618913bc80af61e165a18e3c3ac6
SergGrishkov <serg.grishkov@gmail.com>
2024-03-07 15:28:57 +0200

    Update HW 4

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,6 @@ import ImageModal from "./components/ImageModal/ImageModal";
 import { useState, useEffect } from "react";
 import Modal from "react-modal";
 import clsx from "clsx";
-
-
 import { getPictures } from "./api/controller";
 import styles from "./App.module.css";
 
@@ -16,47 +14,47 @@ const App = () => {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-  const [currPage, setCurrPage] = useState(0);
+  const [currPage, setCurrPage] = useState(1);
   const [hasMorePages, setHasMorePages] = useState(false);
   const [filter, setFilter] = useState("");
-
   const [selectedImage, setSelectedImage] = useState(null);
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  useEffect(() => {
-    Modal.setAppElement("#root");
-  }, []);
+  Modal.setAppElement("#root");
 
-  const updateImages = async (strFilter, page) => {
-    try {
-      setError(false);
-      setLoading(true);
-      const data = await getPictures(strFilter, page);
-      if (data.results.length > 0) {
-        setItems((prevItems) => [...prevItems, ...data.results]);
-        setCurrPage(page);
-        setHasMorePages(page >= data.total_pages ? false : true);
-      } else {
-        setError(true);
-        setHasMorePages(false);
-      }
-    } catch (error) {
-      setError(true);
-      setHasMorePages(false);
-    } finally {
-      setLoading(false);
+  useEffect(() => {
+    if (!filter) {
+      return;
     }
-  };
+
+    const getImages = async () => {
+      try {
+        setLoading(true);
+        const { results } = await getPictures(filter, currPage);
+
+        if (results.length > 0) {
+          setItems((prevImages) => [...prevImages, ...results]);
+          setHasMorePages(true);
+        } else {
+          setHasMorePages(false);
+        }
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getImages();
+  }, [filter, currPage]);
 
   const handleSearch = (inputValue) => {
     setFilter(inputValue);
     setItems([]);
-    setCurrPage(0);
+    setCurrPage(1);
     setHasMorePages(false);
-    updateImages(inputValue, 1);
   };
 
-  const handleMore = () => updateImages(filter, currPage + 1);
+  const handleMore = () => setCurrPage(currPage + 1);
 
   const openModal = (image) => {
     setSelectedImage(image);
@@ -79,6 +77,7 @@ const App = () => {
           isVisible={hasMorePages && !loading}
           onClick={handleMore}
         ></LoadMoreBtn>
+       
       </div>
       <ImageModal
         isOpen={modalIsOpen}
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,29 +1,16 @@
-import { ImageCard } from './ImageCard';
-
+import { ImageCard } from '../ImageCard/ImageCard';
 import styles from './ImageGallery.module.css';
 
 const ImageGallery = ({ images, openModal }) => {
-  const handleImageClick = event => {
-    const clickedCard = event.target.closest('li');
-    if (clickedCard) {
-      const imageId = clickedCard.dataset.imageid;
-      const clickedImageObj = images.find(image => image.id === imageId);
-      if (clickedImageObj) {
-        openModal(clickedImageObj);
-      }
-    }
-  };
   return (
     <>
-      {images.length > 0 && (
-        <ul className={styles.gallery} onClick={handleImageClick}>
-          {images.map(image => (
-            <li key={image.id} data-imageid={image.id}>
-              <ImageCard image={image} />
-            </li>
-          ))}
-        </ul>
-      )}
+      <ul className={styles.gallery}>
+        {images.map((image) => (
+          <li key={image.id} >
+            <ImageCard imageCard={image} onOpen={openModal} />
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
